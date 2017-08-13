a423472d97744649c1cc8fc603909742d56a079b
rodgersk410 <rodgersk410@gmail.com>
2017-08-13 18:06:08 -0400

    save toDoList items to sql server db

---
+++ ToDoList/Data/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using TodoApi.Models;
+using System;
+using System.Linq;
+
+namespace TodoApi.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(TodoContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.TodoItems.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var listItems = new TodoItem[]
+            {
+            new TodoItem{Name="Keith",IsComplete=true},
+            new TodoItem{Name="Rodgers",IsComplete=false},
+            new TodoItem{Name="Alexis",IsComplete=true},
+            };
+            foreach (TodoItem ti in listItems)
+            {
+                context.TodoItems.Add(ti);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -1,21 +1,42 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using TodoApi.Models;
+using TodoApi.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using TodoApi.Models;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace TodoApi
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase());
+
+            var builder = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            // Add framework services.
+            services.AddDbContext<TodoContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, TodoContext context)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
             app.UseMvc();
+
+            DbInitializer.Initialize(context);
         }
     }
 }
